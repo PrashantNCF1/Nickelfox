abbe99f61271a1bea749d5c4826a46fe4d9f3743
PrashantNCF1 <Prashantncf@gmail.com>
2025-01-14 23:53:04 +0530

    Update BaseClass.java
    update pagestretigy

--- src/main/java/com/nickelfox/baseclass/BaseClass.java
+++ src/main/java/com/nickelfox/baseclass/BaseClass.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
