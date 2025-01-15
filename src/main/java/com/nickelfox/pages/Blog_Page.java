2fec92d70916dde0727726a25ebdb4f64637f618
PrashantNCF1 <Prashantncf@gmail.com>
2025-01-15 11:16:46 +0530

    Update Blog_Page.java
    add wait the code

--- src/main/java/com/nickelfox/pages/Blog_Page.java
+++ src/main/java/com/nickelfox/pages/Blog_Page.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Blog_Page {
 
@@ -23,7 +25,9 @@ public class Blog_Page {
 
 	public void go_to_blog(WebDriver driver) throws InterruptedException{
 		
-		Thread.sleep(12000);
+		//Thread.sleep(12000);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		wait.until(ExpectedConditions.elementToBeClickable(blog));
 		blog.click();
 		System.out.println("Go to the blog screen");
 		
