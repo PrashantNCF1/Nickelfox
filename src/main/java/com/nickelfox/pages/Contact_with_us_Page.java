5c0888b820ef6e8016a41d3be5b9ccb178f2f215
PrashantNCF1 <Prashantncf@gmail.com>
2025-01-15 19:19:53 +0530

    Update Contact_with_us_Page.java
    updated import

--- src/main/java/com/nickelfox/pages/Contact_with_us_Page.java
+++ src/main/java/com/nickelfox/pages/Contact_with_us_Page.java
@@ -1,15 +1,13 @@
 package com.nickelfox.pages;
 
 import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.datatransfer.StringSelection;
-import java.awt.event.KeyEvent;
-import org.openqa.selenium.JavascriptExecutor;
+import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class Contact_with_us_Page {
